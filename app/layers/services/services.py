3df740ddf9576802a429293d0fadee6cd1a830f0
Zacarías Samaniego <zacariassamaniego@gmail.com>
2024-11-24 02:50:02 +0000

    Added changes

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -3,13 +3,21 @@
 from ..persistence import repositories
 from ..utilities import translator
 from django.contrib.auth import get_user
+from ..transport import transport
 
 def getAllImages(input=None):
     # obtiene un listado de datos "crudos" desde la API, usando a transport.py.
     json_collection = []
+    if input != None:
+        json_collection = transport.getAllImages(input)
+    else:
+        json_collection = transport.getAllImages()
 
     # recorre cada dato crudo de la colección anterior, lo convierte en una Card y lo agrega a images.
     images = []
+
+    for obj in json_collection:
+        images.append(translator.fromRequestIntoCard(obj))
 
     return images
 
--- app/views.py
+++ app/views.py
@@ -11,17 +11,21 @@ def index_page(request):
 # esta función obtiene 2 listados que corresponden a las imágenes de la API y los favoritos del usuario, y los usa para dibujar el correspondiente template.
 # si el opcional de favoritos no está desarrollado, devuelve un listado vacío.
 def home(request):
-    images = []
+    images = services.getAllImages()
     favourite_list = []
 
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
 
 def search(request):
     search_msg = request.POST.get('query', '')
+    favourite_list = []
 
     # si el texto ingresado no es vacío, trae las imágenes y favoritos desde services.py,
     # y luego renderiza el template (similar a home).
     if (search_msg != ''):
+        images = services.getAllImages(search_msg)
+        return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
+
         pass
     else:
         return redirect('home')
